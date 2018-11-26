ccd5bec7b7e52673f68bcaf78b82cbfa6c325484
Dominic Pearson <dominicsalinger@mac.com>
2018-11-26 09:51:24 -0800

    update

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
     const sheet = new ServerStyleSheet();
     const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
     const styleTags = sheet.getStyleElement();
-    return { ...page, styleTags };
+    return { ...page, styleTags }; // TODO: figure out what this does
   }
 
   render() {
